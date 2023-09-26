dc5087d5ae264559c6709121503e5bb287d46283
Nikolay Gagarinov <feycot@gmail.com>
2023-09-26 21:22:30 +0500

    update

--- src/Module1/GetWithPrefix.php
+++ src/Module1/GetWithPrefix.php
@@ -2,6 +2,11 @@
 
 namespace Fey\DigitalSpectrAcademy\Module1\GetWithPrefix;
 
+
+// Написать функцию, которая принимает в качестве аргументов префикс
+// и массив строк и возвращает все строки, начинающиеся с указанного префикса .
+//Необходимо реализовать функцию наиболее оптимальным образом .
+
 function solution(array $strings, string $prefix): array
 {
     return [];
